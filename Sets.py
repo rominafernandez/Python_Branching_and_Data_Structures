54f6eee1eb3ddd90632d312bdcb304d2ac4190b2
Romina Fernandez <rominafernandez@outlook.de>
2022-10-14 15:28:01 +0200

    Added the solution to exercise 22

--- Sets.py
+++ Sets.py
@@ -15,3 +15,28 @@ print(f"Set 1: {set1}")
 print(f"Set 2: {set2}")
 print(f"Set 3: {set3}")
 print(f"Set 4: {set4}")
+
+
+## Exercise 22 - Adding and removing
+
+# Take your sets from exercise 21
+# Remove some elements from them
+# Try to remove two elements directly one after the other
+# Check which elements were removed with the print command
+# Add the elements again to the set
+# Print out the new sets
+# What changes can you see?
+print("")
+print(set1)
+
+set1.remove(1)
+print(set1)
+
+set1.remove(3)
+set1.remove(0)
+print(set1)
+
+set1.add(1)
+set1.add(3)
+set1.add(0)
+print(set1)
